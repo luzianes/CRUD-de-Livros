1f0001bdde2d1f00451d337b6c5164d18ee1fc07
luzianes <luzianes@gmail.com>
2023-11-09 08:29:32 -0300

    Inclusão de livro

--- CRUD.py
+++ CRUD.py
@@ -3,7 +3,7 @@ os.system ("cls")
 
 biblioteca = {}
 
-print (f"\n### CADASTRO DE LIVROS ###\n")
+print (f"\n### CRUD DE LIVROS ###\n")
 
 #FALTA - não está criando um código para cada item!!!
 
@@ -11,13 +11,13 @@ opcao = int(input ("Escolha a opção desejada: [1] Adicionar, [2] Consultar, [3
 
 if opcao == 1:
 
-    quantidade = input ("Quantos livros deseja adicionar? ")
+    quantidade = int(input ("Quantos livros deseja adicionar? "))
     for i in range (quantidade):
         #coletar dados dos livros e alimentar em um dicionário
         titulo = input (f"Digite o título do livro: ")
         autor = input (f"\nDigite o autor: ")
         categoria = input (f"\nDigite a categoria: ")
-        valor = float(input (f"\nDigite o valor gasto: "))
+        valor = float(input (f"\nDigite o valor pago: "))
         biblioteca[i] = titulo, autor, categoria, valor
     arquivo = open ("biblioteca.csv", "a")  #o parâmetro <a> permite que sejam inseridos dados sem apagar os anteriores
     
@@ -28,12 +28,22 @@ if opcao == 1:
     arquivo.close()
 
 elif opcao == 2:
+    #arquivo = open ("biblioteca.csv", "r")
+    #for linha in arquivo 
     
+
+
+
+
+
     #o trecho a seguir imprime o conteúdo do arquivo .csv
     arquivo = open ("biblioteca.csv", "r")
     for i in arquivo.readlines():
         print (f"\n{i}")
+         
     arquivo.close()
+
+
 
 
 
