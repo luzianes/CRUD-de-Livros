72b3e45d000f200d847faba9aca774a011c93e3a
luzianes <luzianes@gmail.com>
2023-11-17 14:04:24 -0300

    Organização das funções

--- Rascunho.py
+++ Rascunho.py
@@ -7,10 +7,10 @@ biblioteca = {}
 def adicionar(q):
     for i in range (q):
         #Coleta dados dos livros e alimenta em um dicionário
-        titulo = input (f"Digite o título do livro: ")
-        autor = input (f"\nDigite o autor: ")
-        categoria = input (f"\nDigite a categoria: ")
-        valor = input (f"\nDigite o valor pago: ")
+        titulo = input (f"Digite o título do livro: ").capitalize()
+        autor = input (f"Digite o autor: "). capitalize()
+        categoria = input (f"Digite a categoria: ").capitalize()
+        valor = input (f"Digite o valor pago: ").capitalize()
         biblioteca[i] = titulo, autor, categoria, valor
     arquivo = open ("CRUD.txt", "a")  #O parâmetro <a> permite que sejam inseridos dados sem apagar os anteriores
 
@@ -18,17 +18,6 @@ def adicionar(q):
     for i in range (q):
         arquivo.write (f"{biblioteca[i]}\n")
     arquivo.close()
-
-
-print (f"\n### CRUD DE LIVROS ###\n")
-
-opcao = int(input ("Escolha a opção desejada: [1] Adicionar, [2] Consultar, [3] Alterar, [4] Excluir ou [5] Sair: "))
-if opcao == 1:
-
-    quantidade = int(input ("Quantos livros deseja adicionar? "))
-    adicionar(quantidade)
-
-
 
 #Função para consultar livros
 
@@ -41,9 +30,40 @@ if opcao == 1:
 
 
 #Função para excluir livros
-def excluir 
+def excluir ():
+    titulo = input("Digite o nome do livro a ser excluído: ")
 
 
 
 #Função para calcular o total de dinheiro gasto
+
+
+
+
+
+print (f"\n### CRUD DE LIVROS ###\n")
+
+opcao = int(input ("Escolha a opção desejada: [1] Adicionar, [2] Consultar, [3] Alterar, [4] Excluir ou [5] Sair: "))
+if opcao == 1:
+
+    quantidade = int(input ("Quantos livros deseja adicionar? "))
+    adicionar(quantidade)
+
+elif opcao == 2:
+
+
+elif opcao == 3:
+
+
+
+elif opcao == 4:
+
+    excluir () 
+
+elif opcao == 5:
+
+
+
+
+
 
